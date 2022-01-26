8f0dee2ab918e325d2684a8756f1ef35a1c2947e
hehaha68 <908536269@qq.com>
2022-01-26 18:00:53 +0800

    数据结构

--- PB19030861_王湘峰_实验/lab2_离散事件模拟/run.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-./build/test_queue
-./build/test_priority_queue
-valgrind ./build/test_memory
-./build/test_simulation
--- PB19030861_王湘峰_实验/lab2_离散事件模拟/test/test_memory.cc
+++
@@ -1,6 +0,0 @@
-#include "../util/util.h"
-
-int main(void)
-{
-    test_simulation(2, 100, 10, 2, 5, 3);
-}
